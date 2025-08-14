de7f5f310738d8b74ec2f36f17808bc5604f8935
Maximilian Ehlert <maximilian.ehlert@student.htw-berlin.de>
2025-08-14 22:55:37 +0200

    6.3 Replacing conditionals with polymorphisim

--- bottles.py
+++ bottles.py
@@ -3,14 +3,24 @@ class Bottles():
     def __init__(self):
         pass
 
+    @staticmethod
+    def bottle_number_given(number):
+        match number:
+            case 0:
+                cls = BottleNumber0
+            case 1:
+                cls = BottleNumber1
+            case _:
+                cls = BottleNumber
+        return cls(number)
+
     def song(self):
         return self.verses(99, 0)
-
     def verses(self, upper, lower):
         return '\n'.join(self.verse(i) for i in reversed(range(lower, upper + 1)))
     def verse(self, number):
-        bottle_number = BottleNumber(number)
-        next_bottle_number = BottleNumber(bottle_number.successor())
+        bottle_number = self.bottle_number_given(number)
+        next_bottle_number = self.bottle_number_given(bottle_number.successor())
         return (
             f'{bottle_number}'.capitalize() +
             ' of beer on the wall, '
@@ -28,29 +38,34 @@ class BottleNumber:
       return f'{self.quantity()} {self.container()}'
 
   def quantity(self):
-      if self._number == 0:
-          return 'no more'
       return str(self._number)
 
   def container(self):
-      if self._number == 1:
-          return 'bottle'
       return 'bottles'
 
   def action(self):
-      if self._number == 0:
-          return 'Go to the store and buy some more'
       return f'Take {self.pronoun()} down and pass it around'
 
   def pronoun(self):
-      if self._number == 1:
-          return 'it'
       return 'one'
 
   def successor(self):
-      if self._number == 0:
-          return 99
       return self._number - 1
 
 
+class BottleNumber0(BottleNumber):
+    def quantity(self):
+        return 'no more'
 
+    def successor(self):
+        return 99
+
+    def action(self):
+        return 'Go to the store and buy some more'
+
+class BottleNumber1(BottleNumber):
+    def container(self):
+        return 'bottle'
+
+    def pronoun(self):
+        return 'it'
